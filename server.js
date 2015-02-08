9bca83697ea06839732c2c699610618d255f80d1
sleaz0id <kwiatkiwoknie@gmail.com>
2015-02-08 11:57:47 +0100

    Community Rating function added

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.get('/scrape', function(req, res){
               json.title = title;
               json.release = release;
           });
+
+          $('.star-box-giga-star').filter(function() {
+              var data = $(this);
+              rating = data.text();
+              json.rating = rating;
+          });
         }
     });
 });
